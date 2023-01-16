fc6e6c8f8cd06ae68fc29377a93ad998e1e116ea
amanuel <amanuel0000@gmail.com>
2023-01-16 14:57:45 +0300

    add style

--- js/components/Assignment.js
+++ js/components/Assignment.js
@@ -1,9 +1,9 @@
 export default {
     template: `
           <li>
-                <label>
+                <label class="p-2 flex justify-between align-center">
                     {{assignment.name}}
-                    <input type="checkbox" v-model="assignment.complete">
+                    <input type="checkbox" class="ml-4" v-model="assignment.complete">
                 </label>
           </li>
     `,
